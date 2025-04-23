21281882145be23c5816930862e3c1753e4a0974
Mohit <mohitsuthar0703@gmail.com>
2025-04-23 22:39:00 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function App() {
     formData.append("file", file);
 
     try {
-      await axios.post("https://perosnal-cloud-backend-8.onrender.com/upload/upload", formData);
+      await axios.post("https://perosnal-cloud-backend-20.onrender.com/upload/upload", formData);
+
+
 
 
       setFile(null);
